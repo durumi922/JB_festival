a9b0e0793c9d49e406947f24ea2663c5afe6ab05
durumi922 <durumi922@gmail.com>
2024-09-24 10:50:35 +0900

    헤더 추가 디자인 수정

--- food.js
+++ food.js
@@ -1,18 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // 푸드트럭 박스의 토글 기능 구현
-    const foodtruckBox = document.getElementById("foodtruck-box");
-    const toggleButton = foodtruckBox.querySelector(".box-toggle span");
-    const content = foodtruckBox.querySelector(".box-content");
-  
-    toggleButton.addEventListener("click", function() {
-      // 토글: 콘텐츠를 열고 닫음
-      if (content.style.display === "block") {
-        content.style.display = "none";
-        toggleButton.textContent = "▼";
-      } else {
-        content.style.display = "block";
-        toggleButton.textContent = "▲";
-      }
-    });
+  // 푸드트럭 박스의 토글 기능 구현
+  const foodtruckBox = document.getElementById("foodtruck");
+  const toggleButton = foodtruckBox.querySelector(".box-toggle span");
+  const content = foodtruckBox.querySelector(".box-content");
+
+  toggleButton.addEventListener("click", function() {
+    // 토글: 콘텐츠를 열고 닫음
+    if (content.style.display === "block") {
+      content.style.display = "none";
+      toggleButton.textContent = "▼";
+    } else {
+      content.style.display = "block";
+      toggleButton.textContent = "▲";
+    }
   });
-  
+});
